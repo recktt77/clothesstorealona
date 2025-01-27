8cfd42488aa9682f5f24859e2dc70c6cf701d22f
Recktt77 <danik.shaken@gmail.com>
2025-01-27 15:48:21 +0500

    rexuntruct

--- src/components/catalog/catalog.js
+++ src/components/catalog/catalog.js
@@ -1,17 +1,17 @@
 import React from "react";
 import data from './data.json';
-import '../../../node_modules/bootstrap/dist/css/bootstrap.css';
+import Button from "../navigation/button";
 
 class Catalog extends React.Component {
     render() {
         return (
-            <div className="catalog container-fluid">
+            <div className="catalog">
                 {data.map((item) => (
-                    <div className="item col-md-4" id={item.id}>
+                    <div className="item" id={item.id}>
                         <img src={item.images} />
                         <h2 className="title">{item.title}</h2>
                         <h3 className="price">{item.price}</h3>
-                        <button>Add to cart</button>
+                        <Button buttoname='add'/>
                     </div>
                 ))}
             </div>
