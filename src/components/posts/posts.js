73aa5ec7265effcef780bc35f430500a89eb54aa
Sanzhar Nurmukhanbet <n.sanzhar06@gmail.com>
2025-01-28 16:33:36 +0500

    posts corrections

--- src/components/posts/posts.js
+++ src/components/posts/posts.js
@@ -7,11 +7,13 @@ class Posts extends React.Component {
         return (
             <div className="posts">
                 {data.map((item) => (
-                    <div className="item" id={item.id}>
-                        <img src={item.link} />
+                    <div className="container">
                         <h2 className="title">{item.title}</h2>
-                        <h3 className="body">{item.body}</h3>
-                        <h3 className="likes">{item.likes}</h3>
+                        <div className="item" id={item.id}>
+                            <img src={item.link} />
+                            <h3 className="body">{item.body}</h3>
+                            <h3 className="likes">{item.likes}</h3>
+                        </div>
                     </div>
                 ))}
             </div>
