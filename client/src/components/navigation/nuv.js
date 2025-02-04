32bca6b9779fbac080b03bbc925c66cc3262b453
Recktt77 <danik.shaken@gmail.com>
2025-02-04 19:43:32 +0500

    login error degub

--- client/src/components/navigation/nuv.js
+++ client/src/components/navigation/nuv.js
@@ -1,30 +1,29 @@
-import { div } from "framer-motion/client";
 import React from "react";
-import { BrowserRouter as Router, Routes, Route, Link } from "react-router-dom";
+import { Link } from "react-router-dom";
 
-class Navigation extends React.Component{
+class Navigation extends React.Component {
     render() {
         return (
             <div className="nav">
-              {!this.props.isAdmin ? (
-                <nav>
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="/post">Post</Link>
-                <Link to="/ownpage">Ownpage</Link>
-              </nav>
-              )
-              :(<nav>
-                <Link to="/">Home</Link>
-                <Link to="/shop">Shop</Link>
-                <Link to="/post">Post</Link>
-                <Link to="/ownpage">Ownpage</Link>
-                <Link to="/admin">admin</Link>
-              </nav>)
-              }
+                {!this.props.isAdmin ? (
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/shop">Shop</Link>
+                        <Link to="/post">Post</Link>
+                        <Link to="/ownpage">Ownpage</Link>
+                    </nav>
+                ) : (
+                    <nav>
+                        <Link to="/">Home</Link>
+                        <Link to="/shop">Shop</Link>
+                        <Link to="/post">Post</Link>
+                        <Link to="/ownpage">Ownpage</Link>
+                        <Link to="/admin">Admin</Link>
+                    </nav>
+                )}
             </div>
-          );
+        );
     }
 }
 
-export default Navigation
+export default Navigation;
