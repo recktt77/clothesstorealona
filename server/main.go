52c8fce13a91110efaf915fe8b5cf439af391745
Sanzhar Nurmukhanbet <n.sanzhar06@gmail.com>
2025-02-04 13:45:09 +0500

    crud for user with bugs

--- server/main.go
+++ server/main.go
@@ -27,7 +27,9 @@ func main() {
 
 	r.Use(corsMiddleware)
 
-	r.HandleFunc("/users", handlers.getAllUsers).Methods("GET", "OPTIONS")
+	r.HandleFunc("/users", handlers.GetAllUsers).Methods("GET", "OPTIONS")
+	r.HandleFunc("/users/{email}", handlers.UpdateUser).Methods("PUT", "OPTIONS")
+	r.HandleFunc("/users/{email}", handlers.DeleteUser).Methods("DELETE", "OPTIONS")
 
 	r.HandleFunc("/register", handlers.UserRegister).Methods("POST", "OPTIONS")
 	r.HandleFunc("/login", handlers.UserLogin).Methods("POST", "OPTIONS")
