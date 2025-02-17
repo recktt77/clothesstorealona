c67987bd0b9f04e29e479a718e429666145311d2
elliaaan <230745@astanait.edu.kz>
2025-02-17 16:50:14 +0500

    main page

--- client/src/components/main/main.js
+++ client/src/components/main/main.js
@@ -5,8 +5,16 @@ class Main extends React.Component{
     render(){
         return(
             <div className="main">
-                <h1>OUR MAINNN PAGE</h1>
-                <Image/>
+                <div className="images">
+                    <img className="main-page-image" src="https://style.pibig.info/uploads/posts/2023-03/1680127706_style-pibig-info-p-bezhevie-luki-zhenskie-pinterest-11.jpg" />
+                    <img className="main-page-image" src="https://www.myjane.ru/data/cache/2016oct/19/31/624537_81604nothumb650.jpg" />
+                    <img className="main-page-image" src="https://kokoshop.eu/content/images/preces/14335.jpg" />
+                    <img className="main-page-image" src="https://i.pinimg.com/736x/c7/2d/c3/c72dc372f70aba9acf51e5395ae12d58.jpg" />
+                </div>
+                <div className="text">
+                    <h1 className="brand">ALONA</h1>
+                    <p className="desc">Elegance in every detail. Modern fashion inspired by timeless classics</p>
+                </div>
             </div>
         )
     }
